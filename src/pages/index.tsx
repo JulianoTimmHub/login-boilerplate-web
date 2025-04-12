99aa78d1ac67e52a762478745f54309cff305fd8
JulianoTimmHub <julianohenriquetimm@gmail.com>
2025-04-12 02:26:05 -0300

    feat: adding home screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,37 @@
 import { SnackbarMessage } from "@/components/snackbar/SnackbarMessage";
 import { useAuthContext } from "@/hooks/context/useAuthContext.hook";
-import Link from "next/link";
 
 const Home = () => {
-  const { signInResults: { statusSignIn, isLoading }, resetAuthStatus } = useAuthContext();
+  const {
+    signInResults: { statusSignIn, isLoading },
+    userLogout,
+    resetAuthStatus,
+    validateToken
+  } = useAuthContext();
 
   return (
-    <div>
-      <Link href={'/login'}>Ir para o Login</Link>
-      {!isLoading && statusSignIn.color === 'success' && (
-        <SnackbarMessage 
-          status={statusSignIn} 
-          resetStatus={resetAuthStatus} 
-        />
-      )}
+    <div className="container">
+      <div className="col-12 d-flex align-items-center justify-content-center gap-2">
+        {/* <Link href={'/login'}>Ir para o Login</Link> */}
+        <button
+          className="btn btn-primary"
+          onClick={validateToken}
+        >
+          Validar token
+        </button>
+        <button
+          className="btn btn-danger"
+          onClick={userLogout}
+        >
+          Deslogar
+        </button>
+        {!isLoading && statusSignIn.color === 'success' && (
+          <SnackbarMessage
+            status={statusSignIn}
+            resetStatus={resetAuthStatus}
+          />
+        )}
+      </div>
     </div>
   );
 }
