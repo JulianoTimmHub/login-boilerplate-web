09290c866dff4f375ac9e9e3a71d874b0ac7925a
JulianoTimmHub <julianohenriquetimm@gmail.com>
2025-04-12 02:22:13 -0300

    feat: adding new features in User module

--- src/api/user.api.js
+++ src/api/user.api.js
@@ -1,9 +1,15 @@
+import nextConfig from "../../next.config.mjs";
 import { concatUrl } from "./api";
 
-export const registerUser = async ({ username, email, password }) => {
-  const userApi = concatUrl('/user');
+const { applicationName } = nextConfig.env;
+const api = concatUrl('/user');
 
-  return await userApi.post('/registerUser', { username, email, password })
-  .then((res) => res)
-  .catch((res) => res);
+const application = {
+  name: applicationName
+}
+
+export const registerUser = async ({ username, email, password }) => {
+  return await api.post('/registerUser', { username, email, password, application })
+    .then((res) => res)
+    .catch((res) => res);
 }
